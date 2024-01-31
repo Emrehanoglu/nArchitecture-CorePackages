419dc0da291dcb102f4865ad19ef13333fead59e
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-31 23:44:37 +0300

    CorePackages #6

---
+++ Core.Application/Requests/PageRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Requests;
+
+public class PageRequest
+{
+    public int PageIndex { get; set; } //kacıncı sayfa
+    public int PageSize { get; set; } //her sayfada kac data
+}
