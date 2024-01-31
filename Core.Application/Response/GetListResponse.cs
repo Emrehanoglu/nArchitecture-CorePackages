87a3adf1b128814a71033432c8be424a7b8a5f57
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-01 00:49:46 +0300

    CorePackages #7

---
+++ Core.Application/Response/GetListResponse.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistance.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Response;
+
+public class GetListResponse<T>: BasePageableModel
+{
+	private IList<T> _items;
+
+	public IList<T> Items 
+	{ 
+		get => _items??new List<T>(); //items bilgisi yok ise boş bir liste döndür 
+		set => _items = value; 
+	}
+}
---
+++ Core.Persistance/Paging/BasePageableModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistance.Paging;
+
+public abstract class BasePageableModel
+{
+	public int Size { get; set; } //sayfada kaç data var
+	public int Index { get; set; } //hangi sayfadayız
+	public int Count { get; set; } //toplam kayıt sayısı
+	public int Pages { get; set; } //toplam sayfa sayısı
+	public bool HasPrevious { get; set; } //onceki sayfa var mı
+	public bool HasNext { get; set; } //sonraki sayfa var mı
+}
