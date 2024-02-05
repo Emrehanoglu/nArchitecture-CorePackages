768a40307953a202b854119dbeea519a21743560
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-05 07:45:05 +0300

    CorePackages #10

---
+++ Core.CrossCuttingConcerns/Exceptions/Types/BusinessException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Types;
+
+public class BusinessException:Exception
+{
+    public BusinessException() { }
+
+    public BusinessException(string? message) :base(message) { } //base ile Exception 'a da message 'ı gönderdim, message null 'da olabilir.
+
+	public BusinessException(string? message, Exception? innerexception) : base(message,innerexception)	{ }
+}
