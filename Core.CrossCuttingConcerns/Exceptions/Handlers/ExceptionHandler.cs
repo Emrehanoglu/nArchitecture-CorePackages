c310914bb2736e3e2416e04b74d534b9e5c072d1
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-05 20:25:20 +0300

    CorePackages #12

---
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
+
+public abstract class ExceptionHandler
+{
+	//HandleExceptionAsync cağırıldığında gelen Exception type 'ına göre aşağıdaki switch 'lerden birine girecek
+	//BusinessException gibi AuthorizationException,NotFoundException,ValidationException... gibi Exception 'lar olabilir.
+	public Task HandleExceptionAsync(Exception exception) => 
+		exception switch
+		{
+			BusinessException businessException => HandleException(businessException), //gelen Exception, Business type 'ında ise
+			_ => HandleException(exception) //gelen Exception switch içerisindeki Type 'lardan birini karşılamıyor ise
+		};
+
+	//hangi ortam class 'ı ExceptionHandler 'ı inherit ederse aşağıdaki class 'ı kullanarak hata yönetimi yapabilecek.
+	protected abstract Task HandleException(BusinessException businessException); //gelen Exception, Business ise burası calısacak
+	protected abstract Task HandleException(Exception exception); //gelen Exception herhangi bir Type 'ı karsılamıyor ise burası calısacak
+}
---
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Core.CrossCuttingConcerns.Extensions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
+
+public class HttpExceptionHandler : ExceptionHandler
+{
+	private HttpResponse? _response;
+    public HttpResponse Response 
+	{
+		get => _response ?? throw new ArgumentNullException(nameof(_response));
+		set => _response = value; 
+	}
+    protected override Task HandleException(BusinessException businessException)
+	{
+		Response.StatusCode = StatusCodes.Status400BadRequest;
+
+		//bir iş kuralı problemi oluştuğunda JSON formatında bunu dönmek istiyorum
+		string details = new BusinessProblemDetails(businessException.Message).AsJson(); //JSON formatında olması lazım
+
+		return Response.WriteAsync(details);
+	}
+
+	protected override Task HandleException(Exception exception)
+	{
+		throw new NotImplementedException();
+	}
+}
