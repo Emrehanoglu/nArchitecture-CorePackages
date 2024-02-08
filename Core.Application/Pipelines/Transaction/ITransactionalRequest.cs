4a739fba950c1748433432193dfebe2077a36480
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-09 00:17:27 +0300

    CorePackages #17

---
+++ Core.Application/Pipelines/Transaction/ITransactionalRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Transaction;
+
+//sadece imza niyetine kullanacağım için burada boş bir interface olusturdum 
+//bu imza benim işime TransactionScopeBehavior 'a gelecek TRequest 'i imzalarken yarayacak.
+//bir de hangi command 'de kullanıyorsam örneğin CreateBrandCommand classına imza olarak ekledim.
+//çünkü burada CreateBrandCommand benim TransactionScopeBehavior 'daki TRequest 'ime karşılık gelmektedir.
+public interface ITransactionalRequest 
+{
+}
---
+++ Core.Application/Pipelines/Transaction/TransactionScopeBehavior.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Core.Application.Pipelines.Transaction;
+
+//bunun bir pipeline olabilmesi için <TRequset,TResponse> 'u verdim 
+//aynı zamanda bunun bir IPipelineBehavior olması gerekiyor.
+public class TransactionScopeBehavior<TRequset, TResponse> : IPipelineBehavior<TRequset, TResponse>
+	where TRequset : IRequest<TResponse>, ITransactionalRequest
+{
+	public async Task<TResponse> Handle(TRequset request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+	{
+		//async de calıstığım için bu özelliği enable ettim
+		using TransactionScope transactionScope = new(TransactionScopeAsyncFlowOption.Enabled);
+		TResponse response;
+		try
+		{
+			//gelen metodu çalıştır.
+			response = await next();
+
+			//try calıstığı için basarılı olmustur.
+			transactionScope.Complete();
+		}
+		catch (Exception)
+		{
+			transactionScope.Dispose();
+			throw;
+		}
+		return response;
+	}
+}
